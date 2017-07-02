309499150d167859f94395d525cedc44626f9cbb
Elliott Mahler <join.together@gmail.com>
2017-07-02 02:37:47 -0700

    Updated webapp

--- ChartsWebapp/src/main.rs
+++ ChartsWebapp/src/main.rs
@@ -8,7 +8,8 @@ extern crate rocket;
 extern crate postgres;
 extern crate serde_json;
 
-#[macro_use] extern crate rocket_contrib;
+extern crate rocket_contrib;
+
 #[macro_use] extern crate serde_derive;
 
 mod webapp_config;
@@ -21,7 +22,20 @@ use postgres::{Connection, TlsMode};
 use chrono::{DateTime, UTC};
 
 use rocket_contrib::JSON;
-use rocket::response::NamedFile;
+use rocket::response::{content, NamedFile};
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static!{
+	static ref DATABASE_URI: String = env::var("GDQ_DATABASE_URI").unwrap();
+	static ref CURRENT_EVENT_ID: String = env::var("GDQ_LIVE_EVENT_ID").unwrap();
+}
+
+static DONATAION_DATA_QUERY: &'static str = "SELECT id, timestamp, donation_count, donation_total FROM DonationEntry WHERE event_id = $1 ORDER BY timestamp ASC";
+static DONATAION_DATA_UPDATE_QUERY: &'static str = "SELECT id, timestamp, donation_count, donation_total FROM DonationEntry WHERE event_id = $1 AND timestamp > $2 ORDER BY timestamp ASC";
+
+
 
 
 #[derive(Serialize)]
@@ -40,16 +54,18 @@ struct DonationQuery {
 }
 
 #[get("/")]
-fn index() -> String {
-	format!(include_str!("index.html"), static_base=webapp_config::get_static_base())
+fn index() -> content::HTML<String> {
+	let response_str = format!(include_str!("index.html"), static_base=webapp_config::get_static_base());
+
+	content::HTML(response_str)
 }
 
 #[get("/donation_data")]
 fn get_donation_data() -> JSON<DataResponse>  {
-	let database_uri: String = env::var("GDQ_DATABASE_URI").unwrap();
-	let db_connection = Connection::connect(database_uri.as_str(), TlsMode::None).unwrap();
+	let database_uri: &str = &*DATABASE_URI;
+	let db_connection = Connection::connect(database_uri, TlsMode::None).unwrap();
 
-	let query_result = db_connection.query("SELECT id, timestamp, donation_count, donation_total FROM DonationEntry ORDER BY timestamp ASC", &[]).unwrap();
+	let query_result = db_connection.query(DONATAION_DATA_QUERY, &[&*CURRENT_EVENT_ID]).unwrap();
 
 	let result: Vec<DonationEntry> = query_result.iter().map(|row| DonationEntry { timestamp: row.get(1), count: row.get(2), total: row.get(3) }).collect();
 	JSON(DataResponse(result))
@@ -57,11 +73,11 @@ fn get_donation_data() -> JSON<DataResponse>  {
 
 #[get("/donation_data/update?<update_form>")]
 fn get_donation_data_update(update_form: DonationQuery) -> JSON<DataResponse>  {
-	let database_uri: String = env::var("GDQ_DATABASE_URI").unwrap();
-	let db_connection = Connection::connect(database_uri.as_str(), TlsMode::None).unwrap();
+	let database_uri: &str = &*DATABASE_URI;
+	let db_connection = Connection::connect(database_uri, TlsMode::None).unwrap();
 
 	let date_field::DateField(since_date) = update_form.since;
-	let query_result = db_connection.query("SELECT id, timestamp, donation_count, donation_total FROM DonationEntry WHERE timestamp > $1 ORDER BY timestamp ASC", &[&since_date]).unwrap();
+	let query_result = db_connection.query(DONATAION_DATA_UPDATE_QUERY, &[&*CURRENT_EVENT_ID, &since_date]).unwrap();
 
 	let result: Vec<DonationEntry> = query_result.iter().map(|row| DonationEntry { timestamp: row.get(1), count: row.get(2), total: row.get(3) }).collect();
 	JSON(DataResponse(result))
